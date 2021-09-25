5603fd4ea4eb67ed8fb54181641db5221ca8036c
Zain Ul Ebad <p166024@nu.edu.pk>
2021-09-25 16:18:17 +0500

    Merging develop with Cart

--- src/components/UI/categoryUI/CategoryUI.js
+++ src/components/UI/categoryUI/CategoryUI.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import styles from "./CategoryUI.module.css";
 import AddToCartButton from "../../buttons/addCartButton/AddToCartButton.js";
 import DelFromCartButton from "../../buttons/delCartButton/DelFromCartButton.js";
@@ -9,34 +8,6 @@ const CategoryUI = ({ products }) => {
       <div className={styles.inner_collection_wrapper}>
         <h2>CATEGORY DETAIL </h2>
         <p>Top Rated Category</p>
-      </div>
-      <div className={styles.card_wrapper}>
-        {products.map((prod) => {
-          return (
-            <div key={prod.id} className={styles.inner_card}>
-              <div className={styles.image_wrapper}>
-                <img src={prod.image} alt={prod.title} />
-              </div>
-              <div className={styles.desc_wrapper}>
-                <div className={styles.desc_inner_wrapper}>
-                  <p>{prod.title.substring(0, 10)}</p>
-                  <p>$ {prod.price}</p>
-                </div>
-                <div className={styles.desc_inner_wrapper}>
-                  <button className={styles.cart_btn}>Details...</button>
-
-                  <AddToCartButton onClick={triggerToggle} product={prod}>
-                    Add to Cart
-                  </AddToCartButton>
-
-                  <DelFromCartButton onClick={triggerToggle} product={prod}>
-                    Delete
-                  </DelFromCartButton>
-                </div>
-              </div>
-            </div>
-          );
-        })}
       </div>
     </div>
   );
--- src/components/UI/homeCategoriesUI/HomeCategoriesUI.js
+++ src/components/UI/homeCategoriesUI/HomeCategoriesUI.js
@@ -3,36 +3,15 @@ import styles from "./HomeCategoriesUI.module.css";
 
 import { Link } from "react-router-dom";
 
-const HomeCategoriesUI = ({ category, products, addToCartHandler }) => {
+const HomeCategoriesUI = ({ category, products }) => {
   return (
     <div className={styles.collection}>
       <div className={styles.inner_collection_wrapper}>
-        <Link to={`/category/${category}`} className={styles.collection_link}>
-          <h2>{category} COLLECTION </h2>
-        </Link>
+        <h2>{category} COLLECTION </h2>
+
         <p>Top Rated Category</p>
       </div>
-      <div className={styles.card_wrapper}>
-        {products.map((prod) => {
-          return (
-            <div key={prod.id} className={styles.inner_card}>
-              <div className={styles.image_wrapper}>
-                <img src={prod.image} alt={prod.title} />
-              </div>
-              <div className={styles.desc_wrapper}>
-                <div className={styles.desc_inner_wrapper}>
-                  <p>{prod.title.substring(0, 10)}</p>
-                  <p>$ {prod.price}</p>
-                </div>
-                <div className={styles.desc_inner_wrapper}>
-                  <button className={styles.cart_btn}>Details...</button>
-                  <button className={styles.cart_btn}>Add to Cart</button>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <div className={styles.card_wrapper}></div>
     </div>
   );
 };

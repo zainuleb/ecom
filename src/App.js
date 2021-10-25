f96e9e52e5b5604510648402e1eae214ef442446
zainuleb <p166024@nu.edu.pk>
2021-10-26 00:01:27 +0500

    gh-pages deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import "./App.css";
-import { Route, Switch, Redirect } from "react-router-dom";
-import Navbar from "./components/UI/navbarUI/Navbar";
+import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Navbar from './components/UI/navbarUI/Navbar';
 
 //Import Pages
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Categories from "./pages/categories/Categories";
-import ProductDetail from "./pages/productDetail/ProductDetail";
-import Cart from "./pages/cart/Cart";
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Categories from './pages/categories/Categories';
+import ProductDetail from './pages/productDetail/ProductDetail';
+import Cart from './pages/cart/Cart';
 
 //Import Components
-import Footer from "./components/UI/footerUI/Footer";
-import ErrorPage from "./pages/errorPage/ErrorPage.js";
+import Footer from './components/UI/footerUI/Footer';
+import ErrorPage from './pages/errorPage/ErrorPage.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Switch>
-        <Route exact path="/" component={Login} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
         <Route path="/home" component={Home} />
         <Route path="/details/:id" component={ProductDetail} />
         <Route path={`/categories/:category`} component={Categories} />
